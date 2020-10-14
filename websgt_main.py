cbd36695d9d482217ba8621a8c1985a1b231cee9
Brian Lee <bhlee@andrew.cmu.edu>
2020-10-14 10:03:10 -0400

    Added a CSS template
    Changed the home pg

--- websgt_main.py
+++ websgt_main.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
+
+# @app.route("/index")
+# def index():
+#     return render_template("index.html")
 
 @app.route("/temp")
 def temp():
