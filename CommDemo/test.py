ff743b9f61ebbbb31392ed55e45abea7abc813e7
Andrew Cao <andrewjcao@gmail.com>
2020-10-19 11:10:01 -0400

    modifed test format to record time of button press

--- CommDemo/test.py
+++ CommDemo/test.py
@@ -1,4 +1,5 @@
 import serial
+from datetime import datetime
 
 port = "/dev/cu.usbmodem1411"   #port name, probably COM4 on window
 f = open('numPress.txt', 'w');
@@ -9,11 +10,13 @@ count = 0
 while True:
     byte = ard.readline()           #reads byte code ending in line-terminator
     msg = byte.decode("utf-8")      #Convert to string
+    time = datetime.now()
+    f.write('Button pressed at ' + time)
+    count+=1
 
 
     if(count == 10):
         ard.close()                 #Reset string to close port
         f.write('The number of count is ' + str(count) + '\n')
         break
-    count+=1
         
