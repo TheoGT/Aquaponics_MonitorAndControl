6429272297220fe27c64fecc8bb082f4a3d3c4bd
Brian Lee <bhlee@andrew.cmu.edu>
2020-10-26 15:06:16 -0400

    added flow rate subpage

--- SlackBot/SlackBot.py
+++ SlackBot/SlackBot.py
@@ -4,30 +4,35 @@ import getopt
 
 #Send Slack Message Using Slack API
 
-def send_slack_message(message)
+def send_slack_message(message):
+    payload = "{'text':'%s'}" % message
+    response = requests.post("https://hooks.slack.com/services/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",
+        data = payload)
+    print(response.text)
+
 
 def main(argv):
 
-    message = " "
+    message = ""
 
-    try: opts, args = getopt(argv, "hm:", ["message="])
+    try: opts, args = getopt.getopt(argv, "hm:", ["message="])
 
-except getopt.GetoptError:
-    print("SlackMessage.py -m <message>")
-    sys.exit(2)
-
-if len(opt) == 0:
-    messsage = "HELLO WORLD"
-
-for opt, arg in opts: 
-    if opt == "-h":
+    except getopt.GetoptError:
         print("SlackMessage.py -m <message>")
-        sys.exit()
+        sys.exit(2)
 
-    elif opt in ("-m", "--message"):
-        message = arg
+    if len(opts) == 0:
+        messsage = "HELLO WORLD"
 
-send_slack_message(message)
+    for opt, arg in opts: 
+        if opt == "-h":
+            print("SlackMessage.py -m <message>")
+            sys.exit()
+
+        elif opt in ("-m", "--message"):
+            message = arg
+
+    send_slack_message(message)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
