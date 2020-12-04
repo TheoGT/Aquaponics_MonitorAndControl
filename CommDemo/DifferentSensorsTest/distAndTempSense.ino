086b95fb7bae1ee00f4c67c496c7dd32860c89f9
ajcao <32403808+ajcao@users.noreply.github.com>
2020-12-04 11:57:08 -0500

    Add files via upload

---
+++ CommDemo/DifferentSensorsTest/distAndTempSense.ino
@@ -0,0 +1,47 @@
+#include <NewPing.h>
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+#define TRIGGER_PIN_1  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN_1     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+#define ONE_WIRE_BUS 2
+OneWire oneWire(ONE_WIRE_BUS); 
+DallasTemperature sensors(&oneWire);
+
+unsigned long prevTime = millis(); //for time keeping
+
+NewPing sonar = NewPing(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
+
+
+void setup() {
+  Serial.begin(9600); // Open serial monitor at 9600 baud to see ping results.
+   sensors.begin();
+}
+
+void loop() {                    // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  
+    Serial.print("Ping:");
+    Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
+    Serial.print(" ");
+
+    sensors.requestTemperatures();
+    Serial.print("Temp:");
+    Serial.println(sensors.getTempCByIndex(0));
+
+}
+
+bool timeComp()                              
+{
+  unsigned long curTime = millis();
+
+  
+  if (curTime - prevTime >= 10.0)
+  {
+    prevTime = curTime;
+    return true;
+  }
+  return false;
+}
