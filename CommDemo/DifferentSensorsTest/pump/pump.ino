3b55757b6e2f866cdbf5c1f61738d304c0f5ec12
ajcao <32403808+ajcao@users.noreply.github.com>
2020-12-04 11:20:06 -0500

    Update pump.ino

--- CommDemo/DifferentSensorsTest/pump/pump.ino
+++ CommDemo/DifferentSensorsTest/pump/pump.ino
@@ -1,67 +1,83 @@
-//This code was written to be easy to understand.
-									 
-//Modify this code as you see fit.
-//This code will output data to the Arduino serial monitor.
-//Type commands into the Arduino serial monitor to control the EZO-PMP Embedded Dosing Pump.
-//This code was written in the Arduino 1.8.9 IDE
-//An Arduino UNO was used to test this code.
-//This code was last tested 6/2019
+#include <SoftwareSerial.h>                           
+#define rx 2                                          
+#define tx 3  
+//RX = Receive Data
+//TX = Transmit Data
 
-#include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
-#define rx 2                                          //define what pin rx is going to be
-#define tx 3                                          //define what pin tx is going to be
-
-#MOTOR REQURIES 12-24 VOLTS!
-
-SoftwareSerial myserial(rx, tx);                      //define how the soft serial port is going to work
+SoftwareSerial myserial(rx, tx);
+//Put the RX pin first, followed by the TX pin
+//Mnemomic: Listen (receive) before you speak (transmit)
 
 
-String inputstring = "";                              //a string to hold incoming data from the PC
-String devicestring = "";                             //a string to hold the data from the Atlas Scientific product
-boolean input_string_complete = false;                //have we received all the data from the PC
-boolean device_string_complete = false;               //have we received all the data from the Atlas Scientific product
-float ml;                                             //used to hold a floating point number that is the volume 
+String inputstring = "";
+String devicestring = "";
+
+boolean input_string_complete = false;
+boolean device_string_complete = false;
+
+float ml;                                             
 
 
 
-void setup() {                                        //set up the hardware
-  Serial.begin(9600);                                 //set baud rate for the hardware serial port_0 to 9600
-  myserial.begin(9600);                               //set baud rate for the software serial port to 9600
-  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
-  devicestring.reserve(30);                           //set aside some bytes for receiving data from the Atlas Scientific product
+void setup() 
+{                                        	      
+  Serial.begin(9600); 
+  
+  myserial.begin(9600);
+  //Serial used by pump
+	
+  inputstring.reserve(10);                    
+  devicestring.reserve(30);
+}    
+
+
+//Used for inputed command in Arduino serial monitor
+void serialEvent() 
+{                                  		      
+  inputstring = Serial.readStringUntil(13);
+  input_string_complete = true;
 }
 
 
-void serialEvent() {                                  //if the hardware serial port_0 receives a char
-  inputstring = Serial.readStringUntil(13);           //read the string until we see a <CR>
-  input_string_complete = true;                       //set the flag used to tell if we have received a completed string from the PC
-}
-
-
-void loop() {                                         //here we go...
-
-  //Serial.println(String(inputstring));
-  if (input_string_complete == true) {                //if a string from the PC has been received in its entirety
-    myserial.print(inputstring);                      //send that string to the Atlas Scientific product
-    myserial.print('\r');                             //add a <CR> to the end of the string
-    inputstring = "";                                 //clear the string
-    input_string_complete = false;                    //reset the flag used to tell if we have received a completed string from the PC
+void loop() 
+{
+    //User types a command in the serial monitor 
+    if (input_string_complete == true) 
+    {             
+        myserial.print(inputstring);
+        myserial.print('\r');
+        //Command is sent to pump
+	//IF YOU ONLY CARE ABOUT HOW TO USE THE PUMP
+	//THE ABOVE TWO LINES OF CODE ARE TO SEND COMMNANDS
+	 
+	//Removes the user original command
+        inputstring = "";
+	input_string_complete = false;
   }
 
-  if (myserial.available() > 0) {                     //if we see that the Atlas Scientific product has sent a character
-    char inchar = (char)myserial.read();              //get the char we just received
-    devicestring += inchar;                           //add the char to the var called devicestring
-    if (inchar == '\r') {                             //if the incoming character is a <CR>
-      device_string_complete = true;                  //set the flag
+  //Pump sends information back
+  //Exampes of inforamtion are things such as pumping status and total volumed pump
+  if (myserial.available() > 0) 
+    char inchar = (char)myserial.read();
+    devicestring += inchar; 
+    //All the information from the pump has been transmitted
+    if (inchar == '\r') 
+    {
+      device_string_complete = true;
     }
   }
 
-  if (device_string_complete == true) {                           //if a string from the Atlas Scientific product has been received in its entirety
-    Serial.println(devicestring);                                 //send that string to the PC's serial monitor
-    if (isdigit(devicestring[0]) || devicestring[0]== '-') {      //if the first character in the string is a digit or a "-" sign    
-      ml = devicestring.toFloat();                                //convert the string to a floating point number so it can be evaluated by the Arduino   
-       }                                                          //in this code we do not use "ml", But if you need to evaluate the ml as a float, this is how it’s done     
-    devicestring = "";                                            //clear the string
-    device_string_complete = false;                               //reset the flag used to tell if we have received a completed string from the Atlas Scientific product
+  //Prints the information from the pump onto the serial monitor
+  if (device_string_complete == true) 
+  {
+    Serial.println(devicestring); 
+    if (isdigit(devicestring[0]) || devicestring[0]== '-')
+    {
+        ml = devicestring.toFloat();
+    }
+	  
+    //Remove the original data sent by the pump  
+    devicestring = ""; 
+    device_string_complete = false;  
   }
 }
