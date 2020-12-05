1178107c126d17aaa036dca51733d0024559a65b
TheoGT <38996411+TheoGT@users.noreply.github.com>
2020-12-04 22:04:14 -0500

    EC support

--- websgt_main.py
+++ websgt_main.py
@@ -4,6 +4,17 @@ import numpy as np
 
 app = Flask(__name__)
 
+def getlastweek(csv):
+    current = csv[-1,0]
+    #looking for day seven days ago, will not work for may 1-6
+    day = int(current.split("/")[2]) - 7
+    if (day < 1){
+        day += 30
+    }
+    #change range depending on frequency of checks (should be around 1.5* expected checks per week)
+    for i in range(1,100):
+
+
 
 
 @app.route("/")
@@ -11,7 +22,8 @@ def home():
     ambientT = np.genfromtxt('static/csv/temperatures.csv',delimiter=",")
     pH = np.genfromtxt('static/csv/pH.csv',delimiter=",")
     humidity = np.genfromtxt('static/csv/humidity.csv',delimiter=",")
-    return render_template("index.html",ambientTemp = int(ambientT[-1,1]), pH = pH[-1,1], humidity = humidity[-1,1])
+    EC = np.genfromtxt('static/csv/EC.csv',delimiter=",")
+    return render_template("index.html",ambientTemp = int(ambientT[-1,1]), pH = pH[-1,1], humidity = humidity[-1,1], EC = EC[-1,1])
 
 # @app.route("/index")
 # def index():
@@ -40,6 +52,13 @@ def humidity():
     #lastvalue = csv[-1,0]
     #monthmax = lastval % 10000
     return render_template("humidity.html")
+
+@app.route("/EC")
+def EC():
+    csv = np.genfromtxt('static/csv/EC.csv',delimiter = ",")
+    #lastvalue = csv[-1,0]
+    #monthmax = lastval % 10000
+    return render_template("EC.html")
 
 @app.route("/slack")
 def slack():
