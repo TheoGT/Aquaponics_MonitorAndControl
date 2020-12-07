067bc90d0169bcc97f83c6a8c21fe963ab2c9af2
TheoGT <38996411+TheoGT@users.noreply.github.com>
2020-12-07 14:55:16 -0500

    dashboard update

--- websgt_main.py
+++ websgt_main.py
@@ -14,49 +14,60 @@ def getlastweek(csv):
     #change range depending on frequency of checks (should be around 1.5* expected checks per wee
 
 
-
+def getVariables(array, min, max):
+    danger = "progress-bar border-danger"
+    warning = "progress-bar border-warning"
+    safe = "progress-bar border-success"
+    mostRecent = array[-1,1]
+    if (mostRecent < min or mostRecent > max): color = danger
+    elif (mostRecent < min*1.1 or mostRecent > max*.9): color = warning
+    else: color = safe
+    if (np.size(array,axis = 0) > 12): lastHour = array[-13,1]
+    else: lastHour = -1
+    if (np.size(array,axis = 0) > 288): lastDay = array[-288,1]
+    else: lastDay = -1
+    return(color,lastHour,lastDay)
 
 @app.route("/")
 def home():
     ambientT = np.genfromtxt('static/csv/temperatures.csv',delimiter=",")
-    pH = np.genfromtxt('static/csv/pH.csv',delimiter=",")
-    humidity = np.genfromtxt('static/csv/humidity.csv',delimiter=",")
-    EC = np.genfromtxt('static/csv/EC.csv',delimiter=",")
-    return render_template("index.html",ambientTemp = int(ambientT[-1,1]), pH = pH[-1,1], humidity = humidity[-1,1], EC = EC[-1,1])
+    aTval = ambientT[-1,1]
+    (aTcolor,aTlastHour,aTlastDay) = getVariables(ambientT,65,90)
 
-# @app.route("/index")
-# def index():
-#     return render_template("index.html")
+    pH = np.genfromtxt('static/csv/pH.csv',delimiter=",")
+    pHval = pH[-1,1]
+    (pHcolor,pHlastHour,pHlastDay) = getVariables(pH,6,9)
+
+    humidity = np.genfromtxt('static/csv/humidity.csv',delimiter=",")
+    (humidityColor,humidityLastHour,humidityLastDay) = getVariables(humidity,70,90)
+
+    EC = np.genfromtxt('static/csv/EC.csv',delimiter=",")
+    (ecColor,ecLastHour,ecLastDay) = getVariables(EC,.5,2)
+
+    return render_template("index.html",ambientTemp = aTval, pH = pHval, humidity = humidity[-1,1], EC = EC[-1,1],
+    pHcolor = pHcolor, aTcolor = aTcolor, pHlastHour = pHlastHour, pHlastDay = pHlastDay, aTlastday = aTlastDay, aTlasthour = aTlastHour,
+    humidityColor = humidityColor,humidityLastHour = humidityLastHour,humidityLastDay = humidityLastDay,
+    ecColor= ecColor,ecLastHour = ecLastHour,ecLastDay = ecLastDay)
+
 
 @app.route("/Atemp")
 def Atemp():
     csv = np.genfromtxt('static/csv/temperatures.csv',delimiter = ",")
-    #lastvalue = csv[-1,0]
-    #monthmax = lastval % 10000
-    #, monthmin = int(csv[10,0]), monthmax = int(csv[-1,0])
-    return render_template("Temperature(Ambient).html",value = int(csv[-1,1]), csvFile = "../" + csvFileName)
-    #return render_template("temp.html")
+    return render_template("Temperature(Ambient).html",value = int(csv[-1,1]))
 
 @app.route("/pH")
 def pH():
     csv = np.genfromtxt('static/csv/pH.csv',delimiter = ",")
-    #lastvalue = csv[-1,0]
-    #monthmax = lastval % 10000
-    
     return render_template("pH.html")
 
 @app.route("/humidity")
 def humidity():
     csv = np.genfromtxt('static/csv/humidity.csv',delimiter = ",")
-    #lastvalue = csv[-1,0]
-    #monthmax = lastval % 10000
     return render_template("humidity.html")
 
 @app.route("/EC")
 def EC():
     csv = np.genfromtxt('static/csv/EC.csv',delimiter = ",")
-    #lastvalue = csv[-1,0]
-    #monthmax = lastval % 10000
     return render_template("EC.html")
 
 @app.route("/slack")
